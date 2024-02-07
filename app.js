2e965fcd6ca17cebb9f22a5c90d4d9a5e6eb5aed
findtaskin <findtaskin@gmail.com>
2024-02-07 23:40:06 +0600

    task 2

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ function calculateMoney(ticketCount) {
   let result = ticketCount * 120 - (500 + 8 * 50);
   return result;
 }
-console.log(calculateMoney(-130));
+// a, y, i , e , o , u, w
+
+function checkName(givenName) {
+  if (typeof givenName === "string") {
+    let goodDecider = "ayieouw";
+    let goodPointer = givenName.charAt(givenName.length - 1);
+    if (goodDecider.includes(goodPointer.toLowerCase())) {
+      return "Good Name";
+    } else {
+      return "Bad Name";
+    }
+  } else {
+    return "invalid";
+  }
+}
+console.log(checkName());
